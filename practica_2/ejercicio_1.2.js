d1c7dc008f2b06a1f486e8b196d6b75fa333025f
pacopicks <franciscoguerrero0917@gmail.com>
2025-09-22 23:14:28 -0600

    Create ejercicio_1.2.js

---
+++ practica_2/ejercicio_1.2.js
@@ -0,0 +1,10 @@
+const persona = {
+    nombre: "Francisco Guerrero",
+    edad : 20,
+    ciudad: "qro"
+
+}
+
+const { nombre, edad, ciudad } = persona;
+
+console.log("hola me llamo " + nombre + " tengo " + edad + " años y vivo en " + ciudad);
