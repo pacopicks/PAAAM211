1bcc5f4540a4aa43888d2a8a10eebda1b7ee660a
pacopicks <franciscoguerrero0917@gmail.com>
2025-09-23 11:59:08 -0600

    ejercicios p1
    muestran uso de arrow functions y variables let, const y var

---
+++ practica_1/ejercicioB_1.js
@@ -0,0 +1,10 @@
+function cuadrado(numero){
+    return numero*numero;
+
+}
+
+const numero=(numero)=>numero*numero;
+
+console.log(cuadrado(8));
+console.log(cuadrado(2));
+console.log(cuadrado(9));
