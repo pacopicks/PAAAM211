a643fa73e9c0070cf63520c8d9cde6394c907a51
pacopicks <franciscoguerrero0917@gmail.com>
2025-11-06 19:51:12 -0600

    examenprimercommit

--- Examen2doParcial/App.js
+++ Examen2doParcial/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,ScrollView,ImageBackground,Image, requireNativeComponent} from 'react-native';
+
+const ImageBackgroundImage=require("..assets/restaurante.png")
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Estamos trabajando en tu proyecto</Text>
-      <StatusBar style="auto" />
+      
       
     </View>
   );
@@ -15,7 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     
-    
+     
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
---
+++ Examen2doParcial/Imagebackground.js
@@ -0,0 +1,12 @@
+import React, {useState,useEffect } from "react";
+import { StyleSheet,View,Image,Text,ImageBackground,Dimensions } from "react-native";
+
+
+export default function ImageBackground ()  {
+   
+    return (
+      <View>
+        <Text>Proximamente.....</Text>
+      </View>
+    )
+}
